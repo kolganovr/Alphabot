e8de65f63e7e7d6ce691b82235c9b67d325000b7
Roman Kolganov <rakolganov@mai.education>
2023-03-03 18:00:45 +0300

    camera image
    shows image from camera using classes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <math.h>
+#include <tuple>
 
-// This enum represents the actions that the Alphabot can perform.
+using namespace cv;
+using namespace std;
+
+#define MIN_CONTOUR_AREA 250
+#define M_PI 3.1416
+
+// Действия которые может выполнять робот
 enum Action
 {
     FORWARD,
@@ -10,76 +19,124 @@ enum Action
     CLEAN
 };
 
-// This class is used to control the movement of the robot.
+// Класс для управления двигателями
 class EngineController
 {
 public:
-    // Moves the robot forward for the specified time.
+    // Выполняет движение вперед заданное время
     void moveForward(int time);
-    // Moves the robot backwards for the specified time.
+    // Выполняет движение назад заданное время
     void moveBackwards(int time);
-    // Moves the robot left for the specified time.
+    // Выполняет поворот налево заданное время
     void moveLeft(int time);
-    // Moves the robot right for the specified time.
+    // Выполняет поворот направо заданное время
     void moveRight(int time);
 };
 
-// This class is used to control the cleaning ability of robot.
+// Класс для управления моющими щетками
 class Cleaner
 {
 public:
-    // Performs the cleaning action.
+    // Выполняет очистку поверхности
     void perform();
 };
 
-// Class for getting the image and transfering it to server.
+// Класс для управления камерой
 class Camera
-{   
+{
+private:
+    // VideoCapture объект для считывания изображения с камеры
+    VideoCapture cap;
+
+    /// Получает изображение с камеры
+    /// @return Кортеж из двух матриц -- HSV и frame
+    Mat getImage()
+    {
+        // Считываем кадр
+        Mat frame;
+        cap.read(frame);
+
+        // Переводим кадр в HSV
+        Mat hsv;
+        cvtColor(frame, hsv, COLOR_BGR2HSV);
+
+        // Отображаем исходный кадр
+        imshow("Original", frame);
+
+        // Возвращаем HSV
+        return hsv;
+    }
+
 public:
-    // Sends the image to the server.
-    void sendToServer(); 
+    Camera()
+    {
+        // Инициализируем камеру
+        cap.open(0);
+        // Задаём размеры изображения
+        cap.set(CAP_PROP_FRAME_WIDTH, 640);
+        cap.set(CAP_PROP_FRAME_HEIGHT, 480);
+
+        // Создаем окна для отображения исходного кадра и фильтрованного
+        namedWindow("Original", WINDOW_NORMAL);
+        // namedWindow("Filtered", WINDOW_NORMAL);
+    }
+
+    ~Camera()
+    {
+        // Закрываем камеру
+        cap.release();
+        destroyAllWindows();
+    }
+
+    /// Отправляем изображение на сервер
+    /// @return Кортеж из двух матриц -- HSV и frame
+    Mat sendToServer()
+    {
+        return getImage();
+    }
 };
 
-// This class represents a graffiti
-// It has a position and a boolean that tells if it exists or not
-class Graffiti{
-    bool exist = false; // Does graffiti exist
-    int posX; // X position of graffiti
-    int posY; // Y position of graffiti
+// Класс обьъекта граффити
+class Graffiti
+{
+    bool exist = false; // Существование граффити
+    int posX;           // X позиция граффити
+    int posY;           // Y позиция граффити
 public:
-    // Constructor
+    Graffiti() {}
     Graffiti(int x, int y)
     {
         posX = x;
         posY = y;
         exist = true;
     }
-    
-    /// Returns X position of graffiti
-    /// @return position (0 is Left side (example!) )
+
+    /// Возвращает X позицию граффити
+    /// @return Позиция X -- 0 Левая сторона
     int getPosX() { return posX; }
-    /// Returns Y position of graffiti
-    /// @return position (0 is Down side (example!) )
+    /// Возвращает Y позицию граффити
+    /// @return Позиция Y -- 0 верхняя сторона
     int getPosY() { return posY; }
 };
 
+// Класс робота
 class Alphabot
 {
 private:
-    bool engineStarted; 
+    bool engineStarted;
     EngineController engineController;
     Cleaner cleaner;
-    int posX; // Current X coordinate
-    int posY; // Current Y coordinate
-    Graffiti target; // Current graffiti target
+    int posX;        // Текущая X координата
+    int posY;        // Текущая Y координата
+    Graffiti target; // Текущая позиция цели
 
-    /// Choosing Actiion based on Graffiti pos 
-    /// @param curTarget object we are heading to
-    /// @return Chosed Action
+    /// Выбирает действие на основании позиции граффити
+    /// @param curTarget объект к которому движемся
+    /// @return выбранное действие
     Action chooseAction(Graffiti curTarget);
 
-    // Move robot, or clean graffiti.
-    void doAction(Action action, int time) 
+    // Движение робота или очистка
+    void doAction(Action action, int time)
     {
         switch (action)
         {
@@ -102,10 +159,17 @@ private:
             break;
         }
     }
+
 public:
-    /// gets a messsage from the Server
-    /// @param target graffity object robot needs to go to
-    void recieveMessage(Graffiti graffiti){
+    Alphabot() {
+        engineStarted = false;
+        posX = 0;
+        posY = 0;
+    }
+    /// Получает сообщенеи с сервера
+    /// @param target граффити к кторому нужно двигаться
+    void recieveMessage(Graffiti graffiti)
+    {
         target = graffiti;
     }
     void startEngine()
@@ -118,17 +182,68 @@ public:
     }
 };
 
-// This class represents the server that is used to communicate between the robot and the camera.
+// Класс сервера для коммуникации между камерой и роботом а также вычислений
 class Server
 {
 private:
     Alphabot alphabot;
-    /// Searching for Graffiti positions
-    /// @return Graffiti object
+    Camera camera;
+    Mat hsv;
+    // Mat result;
+
+    /// Ищет граффити
+    /// @return Graffiti объект граффити
     Graffiti searchForGraffiti();
+
+    void showResults()
+    {
+        // Отображаем результаты
+        imshow("Original", hsv);
+        // imshow("Filtered", result);
+    }
+
 public:
-    // Receives messages from the user.
-    void receiveMessage();
-    // Sends messages to the user.
-    void sendMessage(Graffiti graffiti);
+    Server()
+    {
+        // Инициализируем камеру
+        camera = Camera();
+    }
+    // Получает сообщение от камеры, отправляет роботу и отображает результаты
+    void receiveMessage()
+    {
+        // Получаем изображение с камеры
+        Mat hsv = camera.sendToServer();
+
+        // Ищем граффити
+        // Graffiti graffiti = searchForGraffiti();
+
+        // Отображаем результаты
+        // showResults();
+
+        // Отправляем сообщение роботу
+        // sendMessage(graffiti);
+    }
+    // Отправляет сообщение роботу
+    void sendMessage(Graffiti graffiti)
+    {
+        alphabot.recieveMessage(graffiti);
+    }
 };
+
+int main()
+{
+    Server server;
+    Camera camera;
+    while (true)
+    {
+        // Получаем сообщение от камеры
+        // server.receiveMessage();
+        camera.sendToServer();
+
+        // Ожидаем нажатия клавиши
+        // При нажатии на esc выходим
+        if (waitKey(1) == 27)
+            break;
+    }
+    return 0;
+}
